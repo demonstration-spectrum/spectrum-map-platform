6cabd24ec9cfe5c57c51e64a69dd4fe4a73fe2fe
Nampd <demonstration.spectrum@gmail.com>
2025-10-10 14:45:27 +1100

    fix rxjs dependencies mismatch

--- apps/backend/src/datasets/file-upload.interceptor.ts
+++ apps/backend/src/datasets/file-upload.interceptor.ts
@@ -6,8 +6,7 @@ import { FileUploadService } from './file-upload.service';
 @Injectable()
 export class FileUploadInterceptor implements NestInterceptor {
   constructor(private readonly fileUploadService: FileUploadService) {}
-
-  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> | Promise<Observable<any>> {
     const multerConfig = this.fileUploadService.getMulterConfig();
     const InterceptorClass = FileInterceptor('file', multerConfig);
     const interceptorInstance: NestInterceptor = new InterceptorClass(this.fileUploadService as any);
